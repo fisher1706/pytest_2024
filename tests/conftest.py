ab8c4c0b5da28264ecad056e89ccf14975bbf07b
Oleh Lazeba <oleh.lazeba@cd.p2h.com>
2024-01-22 14:05:43 +0200

    lesson_11

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from src.config import settings
+
+# TODO: 1 - use "pytest.ini" - env_files = test.env or command: pytest --envfile .test.env - use: pytest-dotenv
+
+
+@pytest.fixture(
+    scope="session",
+    autouse=True
+)
+def setup_db():
+    print(f"\n{settings.MODE}")
+    return "text_one"
+
+
+@pytest.fixture()
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+# def pytest_addoption(parser):
+#     parser.addoption(
+#         "--browser",
+#         default="chrome",
+#         choices=("chrome", "firefox")
+#     )
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--run-slow",
+        default=True,
+        choices=("True", "False")
+    )
