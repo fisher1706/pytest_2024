538f8bb8724b0436518d6593265ba190e744336b
Oleh Lazeba <oleh.lazeba@cd.p2h.com>
2024-01-24 23:13:21 +0200

    update conftest file

--- src/config.py
+++ src/config.py
@@ -29,20 +29,20 @@ class PydanticSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
 
-class PydanticSettingsConfig(BaseSettings):
-    MODE: str
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
-
-    @property
-    def DB_URL(self):
-        return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-    class Config:
-        env_file = ".env"
+# class PydanticSettingsConfig(BaseSettings):
+#     MODE: str
+#     DB_HOST: str
+#     DB_PORT: int
+#     DB_USER: str
+#     DB_PASS: str
+#     DB_NAME: str
+#
+#     @property
+#     def DB_URL(self):
+#         return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+#
+#     class Config:
+#         env_file = ".env"
 
 
 settings = PydanticSettings()
@@ -52,8 +52,8 @@ if __name__ == '__main__':
     sl = SettingsLoadDotEnv()
     print(sl.DB_URL)
 
-    pc = PydanticSettingsConfig()
-    print(pc.DB_URL)
+    # pc = PydanticSettingsConfig()
+    # print(pc.DB_URL)
 
     ps = PydanticSettings()
     print(ps.DB_URL)
