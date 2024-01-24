5b0f965016336e5fab07e4fd66cd0721ad39a9ea
Oleh Lazeba <oleh.lazeba@cd.p2h.com>
2024-01-24 23:33:23 +0200

    update config file

--- src/config.py
+++ src/config.py
@@ -14,19 +14,19 @@ class SettingsLoadDotEnv:
     DB_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
-class PydanticSettings(BaseSettings):
-    MODE: str
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
-
-    @property
-    def DB_URL(self):
-        return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-    model_config = SettingsConfigDict(env_file=".env")
+# class PydanticSettings(BaseSettings):
+#     MODE: str
+#     DB_HOST: str
+#     DB_PORT: int
+#     DB_USER: str
+#     DB_PASS: str
+#     DB_NAME: str
+#
+#     @property
+#     def DB_URL(self):
+#         return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+#
+#     model_config = SettingsConfigDict(env_file=".env")
 
 
 # class PydanticSettingsConfig(BaseSettings):
@@ -45,7 +45,7 @@ class PydanticSettings(BaseSettings):
 #         env_file = ".env"
 
 
-settings = PydanticSettings()
+# settings = PydanticSettings()
 
 
 if __name__ == '__main__':
@@ -55,5 +55,5 @@ if __name__ == '__main__':
     # pc = PydanticSettingsConfig()
     # print(pc.DB_URL)
 
-    ps = PydanticSettings()
-    print(ps.DB_URL)
+    # ps = PydanticSettings()
+    # print(ps.DB_URL)
